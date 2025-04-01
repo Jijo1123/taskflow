da6ac48f23d6ab0dcbe8b101aa191df0dacc056a
Jijo1123 <aotieno584@gmail.com>
2025-04-01 06:06:20 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 
-import  from "../frontend/src/index"
+import  from "../frontend/src/App"
 
 export default function SyntheticV0PageForDeployment() {
-  return < />
+  return <App />
 }
